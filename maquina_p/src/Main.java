44d28f1728c54d5171247e274eb7a4f2b5ca94a0
HarZe <dserranoarce@hotmail.com>
2014-05-05 16:14:38 +0200

    Comprobado el funcionamiento con nuevos ejemplos

--- maquina_p/src/Main.java
+++ maquina_p/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			MaquinaP maquina = new MaquinaP(Parser.instDeArchivo("input.txt"));
+			MaquinaP maquina = new MaquinaP(Parser.instDeArchivo(args[0]));
 			maquina.ejecutar();
 		} catch (Exception e) {
 			e.printStackTrace();
